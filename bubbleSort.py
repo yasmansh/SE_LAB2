83115aadc21a9393be9c8de1d52e606c3a8916c2
yasaman <45336465+yasmansh@users.noreply.github.com>
2022-11-06 18:24:13 +0330

    Add bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,18 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        flag = 0
+
+        for j in range(n - 1):
+
+            if arr[j] > arr[j + 1]:
+                tmp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
+                flag = 1
+
+        if flag == 0:
+            break
+
+    return arr
